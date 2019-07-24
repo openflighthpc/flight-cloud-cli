8de5c0dfc0a78c5f9035461a8d4f4ec301cceb24
Andrew <andrew.toghill@alces-software.com>
2019-07-24 12:41:35 +0100

    Refactor to work with undeployed nodes

--- lib/cloudcli/commands/power.rb
+++ lib/cloudcli/commands/power.rb
@@ -46,7 +46,9 @@ module CloudCLI
               .body
               .to_h
 
-        result['nodes'].sort.each do |node, status|
+        nodes = result['nodes'].merge(result['errors'])
+        nodes.sort.each do |node, status|
+          status = 'undeployed' if status.include? " "
           puts "#{node}: #{status}"
         end
       end
